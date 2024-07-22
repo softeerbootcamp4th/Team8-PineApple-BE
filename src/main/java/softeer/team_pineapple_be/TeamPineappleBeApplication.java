1b6bd4fc6eba329c1ae06209839053a5b9a70327
devchlee12 <devchlee12@gmail.com>
2024-07-22 18:06:15 +0900

    [FEAT] 프로젝트 시작

---
+++ src/main/java/softeer/team_pineapple_be/TeamPineappleBeApplication.java
@@ -0,0 +1,13 @@
+package softeer.team_pineapple_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPineappleBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TeamPineappleBeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/softeer/team_pineapple_be/TeamPineappleBeApplicationTests.java
@@ -0,0 +1,13 @@
+package softeer.team_pineapple_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamPineappleBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
