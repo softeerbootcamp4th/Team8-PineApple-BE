b719631bdf964cf6cbc4e58e2440cc299dfb997b
repitor222 <repitor15@gmail.com>
2024-07-25 17:24:15 +0900

    [FEAT] 퀴즈 컨텐트 응답을 담당하는 QuizContentResponse 클래스 생성
    description, answer 내용을 담은 응답 형식

---
+++ src/main/java/softeer/team_pineapple_be/quiz/response/QuizContentResponse.java
@@ -0,0 +1,34 @@
+package softeer.team_pineapple_be.quiz.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import softeer.team_pineapple_be.quiz.domain.QuizContent;
+
+/**
+ * QuizContent의 응답을 구성하는 클래스
+ */
+@AllArgsConstructor
+@Getter
+public class QuizContentResponse {
+
+    private String quizDescription;
+    private String quizQuesion1;
+    private String quizQuesion2;
+    private String quizQuesion3;
+    private String quizQuesion4;
+
+    /**
+     * QuizContent의 엔티티를 응답 형식으로 변환하는 메서드
+     * @param quizContent 변환하고자 하는 QuizContent
+     * @return 응답 형식에 맞는 QuizContent
+     */
+    public static QuizContentResponse quizContentResponse(QuizContent quizContent){
+        return new QuizContentResponse(
+                quizContent.getQuizDescription(),
+                quizContent.getQuizQuestion1(),
+                quizContent.getQuizQuestion2(),
+                quizContent.getQuizQuestion3(),
+                quizContent.getQuizQuestion4());
+    }
+}
