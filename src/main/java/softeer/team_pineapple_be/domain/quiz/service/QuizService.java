9bb9086d72f631e5a452dd767a982b8959ee46e3
repitor222 <repitor15@gmail.com>
2024-07-28 20:22:01 +0900

    [feat] QuizHistory 메서드 등록을 위한 QuizService 수정
    quizHistory 메서드 생성을 통한 퀴즈 참여 정보 등록
    
    Transactional 설정
    
    javadoc등록

--- src/main/java/softeer/team_pineapple_be/domain/quiz/service/QuizService.java
+++ src/main/java/softeer/team_pineapple_be/domain/quiz/service/QuizService.java
@@ -1,13 +1,19 @@
 package softeer.team_pineapple_be.domain.quiz.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import softeer.team_pineapple_be.domain.member.domain.Member;
+import softeer.team_pineapple_be.domain.member.repository.MemberRepository;
 import softeer.team_pineapple_be.domain.quiz.domain.QuizContent;
+import softeer.team_pineapple_be.domain.quiz.domain.QuizHistory;
 import softeer.team_pineapple_be.domain.quiz.domain.QuizInfo;
 import softeer.team_pineapple_be.domain.quiz.repository.QuizContentRepository;
+import softeer.team_pineapple_be.domain.quiz.repository.QuizHistoryRepository;
 import softeer.team_pineapple_be.domain.quiz.repository.QuizInfoRepository;
 import softeer.team_pineapple_be.domain.quiz.request.QuizInfoRequest;
 import softeer.team_pineapple_be.domain.quiz.response.QuizContentResponse;
+import softeer.team_pineapple_be.domain.quiz.response.QuizHistoryResponse;
 import softeer.team_pineapple_be.domain.quiz.response.QuizInfoResponse;
 
 import java.time.LocalDate;
@@ -24,6 +30,8 @@ public class QuizService {
 
     private final QuizContentRepository quizContentRepository;
     private final QuizInfoRepository quizInfoRepository;
+    private final QuizHistoryRepository quizHistoryRepository;
+    private final MemberRepository memberRepository;  //멤버 서비스가 생성될 시에 리팩토링
 
     //TODO: 선착순 처리 되어야함(선착순 처리에 대한 정보 전송할 시에 Response까지 수정되어야 함.)
 
@@ -32,6 +40,7 @@ public class QuizService {
      * @param quizInfoRequest 퀴즈 번호와 사용자가 제출한 정답을 받아오기 위한 객체
      * @return 정답 안내 정보에 대한 내용
      */
+    @Transactional
     public QuizInfoResponse quizIsCorrect(QuizInfoRequest quizInfoRequest) {
         Optional<QuizInfo> optionalQuizInfo = quizInfoRepository.findById(quizInfoRequest.getQuizId());
         if (optionalQuizInfo.isEmpty()) {
@@ -48,8 +57,28 @@ public class QuizService {
      * 현재 날짜에 대한 이벤트 내용을 전송해주는 메서드
      * @return 현재 날짜의 이벤트 내용
      */
+    @Transactional
     public QuizContentResponse quizContent() {
         QuizContent quizContent = quizContentRepository.findByQuizDate(LocalDate.now());
         return QuizContentResponse.of(quizContent);
+    }
+
+    /**
+     * 퀴즈 참여 여부를 저장하는 메서드
+     * @param phoneNumber 참여 여부를 등록하고자 하는 사용자
+     * @return 참여 여부가 등록된 사용자의 툴박스 개수
+     */
+    @Transactional
+    public QuizHistoryResponse quizHistory(String phoneNumber) {
+        QuizHistory quizHistory = quizHistoryRepository.findByMemberPhoneNumberAndParticipantDate(phoneNumber, LocalDate.now());
+        if(quizHistory != null){
+            return null;
+        }
+        Member member = memberRepository.findByPhoneNumber(phoneNumber);
+        member.incrementToolBoxCnt();
+        memberRepository.save(member);
+        quizHistory = new QuizHistory(member);
+        quizHistoryRepository.save(quizHistory);
+        return QuizHistoryResponse.of(member);
     }
 }
