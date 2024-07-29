3f9fc47559815f104b5f98e0be202abf20e2eb23
devchlee12 <devchlee12@gmail.com>
2024-07-29 17:22:48 +0900

    [FEAT] 기대평 복합 키 클래스 생성

---
+++ src/main/java/softeer/team_pineapple_be/domain/comment/domain/CommentLike.java
@@ -0,0 +1,18 @@
+package softeer.team_pineapple_be.domain.comment.domain;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import softeer.team_pineapple_be.domain.comment.domain.id.LikeId;
+
+/**
+ * 기대평 좋아요 이력
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+public class CommentLike {
+  @EmbeddedId
+  private LikeId id;
+}
