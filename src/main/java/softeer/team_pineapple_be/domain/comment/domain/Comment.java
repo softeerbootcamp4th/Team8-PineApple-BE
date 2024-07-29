5a83185ce55ce84015fb6f254181014a3ba57540
devchlee12 <devchlee12@gmail.com>
2024-07-29 17:23:31 +0900

    [FEAT] 기대평 엔티티, 좋아요 엔티티 생성

---
+++ src/main/java/softeer/team_pineapple_be/domain/comment/domain/Comment.java
@@ -0,0 +1,30 @@
+package softeer.team_pineapple_be.domain.comment.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 댓글 엔티티
+ */
+@Entity
+@NoArgsConstructor
+@Getter
+public class Comment {
+  @Id
+  @Column(nullable = false)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(nullable = false)
+  private Long memberId;
+  @Column(nullable = false)
+  private String content;
+  @Column(nullable = false)
+  private LocalDateTime createdDate;
+}
