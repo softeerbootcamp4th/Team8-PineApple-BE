f4a6afedc3badd4725dfda563391b1a9d090192e
devchlee12 <devchlee12@gmail.com>
2024-07-30 11:42:37 +0900

    [FIX] 좋아요 리포지토리의 키를 복합키로 변경

--- src/main/java/softeer/team_pineapple_be/domain/comment/repository/CommentLikeRepository.java
+++ src/main/java/softeer/team_pineapple_be/domain/comment/repository/CommentLikeRepository.java
@@ -3,9 +3,10 @@ package softeer.team_pineapple_be.domain.comment.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import softeer.team_pineapple_be.domain.comment.domain.CommentLike;
+import softeer.team_pineapple_be.domain.comment.domain.id.LikeId;
 
 /**
  * 좋아요 리포지토리
  */
-public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+public interface CommentLikeRepository extends JpaRepository<CommentLike, LikeId> {
 }
