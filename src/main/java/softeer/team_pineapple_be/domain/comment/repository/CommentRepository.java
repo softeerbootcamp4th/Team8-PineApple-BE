09da1ab9b7300057cf679c86fae065845c06ade7
devchlee12 <devchlee12@gmail.com>
2024-07-30 12:39:49 +0900

    [FEAT] 기대평 DB에서 날짜별로 찾는 메서드 추가

--- src/main/java/softeer/team_pineapple_be/domain/comment/repository/CommentRepository.java
+++ src/main/java/softeer/team_pineapple_be/domain/comment/repository/CommentRepository.java
@@ -1,5 +1,7 @@
 package softeer.team_pineapple_be.domain.comment.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +15,8 @@ import softeer.team_pineapple_be.domain.comment.domain.Comment;
  * 기대평 리포지토리
  */
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+  Page<Comment> findAllByPostTimeBetween(Pageable pageable, LocalDateTime startOfDay, LocalDateTime endOfDay);
+
   @Query(
       "SELECT c FROM Comment c WHERE c.phoneNumber = :phoneNumber AND c.postTime >= :startOfDay AND c.postTime < :endOfDay")
   Optional<Comment> findCommentsByAuthorAndDate(@Param("phoneNumber") String phoneNumber,
