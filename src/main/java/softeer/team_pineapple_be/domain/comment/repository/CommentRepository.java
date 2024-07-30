a41cc7c44cae4734a0a8ab77aa4f3dc7fb9df6a8
devchlee12 <devchlee12@gmail.com>
2024-07-30 11:17:32 +0900

    [FIX] 주어진 시간 사이에 유저가 작성한 기대평 가져오는 JPQL 구현

--- src/main/java/softeer/team_pineapple_be/domain/comment/repository/CommentRepository.java
+++ src/main/java/softeer/team_pineapple_be/domain/comment/repository/CommentRepository.java
@@ -1,6 +1,11 @@
 package softeer.team_pineapple_be.domain.comment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 import softeer.team_pineapple_be.domain.comment.domain.Comment;
 
@@ -8,4 +13,8 @@ import softeer.team_pineapple_be.domain.comment.domain.Comment;
  * 기대평 리포지토리
  */
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+  @Query(
+      "SELECT c FROM Comment c WHERE c.phoneNumber = :phoneNumber AND c.postTime >= :startOfDay AND c.postTime < :endOfDay")
+  Optional<Comment> findCommentsByAuthorAndDate(@Param("phoneNumber") String phoneNumber,
+      @Param("startOfDay") LocalDateTime startOfDay, @Param("endOfDay") LocalDateTime endOfDay);
 }
