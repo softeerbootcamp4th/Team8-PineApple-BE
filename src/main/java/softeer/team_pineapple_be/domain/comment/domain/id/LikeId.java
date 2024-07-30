4ffede349464981f6cb29610c8eb48d0c673bacf
devchlee12 <devchlee12@gmail.com>
2024-07-30 11:44:20 +0900

    [FIX] 좋아요 엔티티 전화번호 String 으로 변경

--- src/main/java/softeer/team_pineapple_be/domain/comment/domain/id/LikeId.java
+++ src/main/java/softeer/team_pineapple_be/domain/comment/domain/id/LikeId.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,10 +15,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @Embeddable
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 public class LikeId implements Serializable {
   @Column(nullable = false)
   private Long commentId;
   @Column(nullable = false)
-  private Long phoneNumber;
+  private String phoneNumber;
 }
