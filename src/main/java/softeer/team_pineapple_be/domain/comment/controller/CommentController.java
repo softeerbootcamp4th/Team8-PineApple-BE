5ae8220d5b2b5d4b1c979b73725db559631e651f
devchlee12 <devchlee12@gmail.com>
2024-07-30 13:28:13 +0900

    [FEAT] Swagger 설명 추가

--- src/main/java/softeer/team_pineapple_be/domain/comment/controller/CommentController.java
+++ src/main/java/softeer/team_pineapple_be/domain/comment/controller/CommentController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import softeer.team_pineapple_be.domain.comment.request.CommentLikeRequest;
 import softeer.team_pineapple_be.domain.comment.request.CommentRequest;
@@ -20,24 +22,28 @@ import softeer.team_pineapple_be.global.common.response.SuccessResponse;
 /**
  * 기대평 컨트롤러
  */
+@Tag(name = "기대평 API", description = "기대평 기능 처리")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/comments")
 public class CommentController {
   private final CommentService commentService;
 
+  @Operation(summary = "기대평 남기기")
   @PostMapping
   public ResponseEntity<SuccessResponse> addComment(@RequestBody CommentRequest commentRequest) {
     commentService.saveComment(commentRequest);
     return ResponseEntity.ok(new SuccessResponse());
   }
 
+  @Operation(summary = "좋아요 누르기")
   @PostMapping("/likes")
   public ResponseEntity<SuccessResponse> addLikes(@RequestBody CommentLikeRequest commentLikeRequest) {
     commentService.saveCommentLike(commentLikeRequest);
     return ResponseEntity.ok(new SuccessResponse());
   }
 
+  @Operation(summary = "page, 정렬순서, 날짜에 따라 기대평 가져오기")
   @GetMapping
   public ResponseEntity<CommentPageResponse> getComments(
       @RequestParam(required = false, defaultValue = "0") Integer page,
