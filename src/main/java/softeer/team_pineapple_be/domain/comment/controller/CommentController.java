b726913e75fa8043cde3f5dfe438a1541b780ec4
devchlee12 <devchlee12@gmail.com>
2024-07-30 11:16:49 +0900

    [FIX] 테스트용 출력문 삭제

--- src/main/java/softeer/team_pineapple_be/domain/comment/controller/CommentController.java
+++ src/main/java/softeer/team_pineapple_be/domain/comment/controller/CommentController.java
@@ -1,13 +1,18 @@
 package softeer.team_pineapple_be.domain.comment.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import softeer.team_pineapple_be.domain.comment.request.CommentRequest;
 import softeer.team_pineapple_be.domain.comment.response.CommentPageResponse;
 import softeer.team_pineapple_be.domain.comment.service.CommentService;
+import softeer.team_pineapple_be.global.common.response.SuccessResponse;
 
 /**
  * 기대평 컨트롤러
@@ -18,14 +23,20 @@ import softeer.team_pineapple_be.domain.comment.service.CommentService;
 public class CommentController {
   private final CommentService commentService;
 
+  @PostMapping
+  public ResponseEntity<SuccessResponse> addComment(@RequestBody CommentRequest commentRequest) {
+    commentService.saveComment(commentRequest);
+    return ResponseEntity.ok(new SuccessResponse());
+  }
+
   @GetMapping
-  public CommentPageResponse getComments(@RequestParam(required = false, defaultValue = "0") Integer page,
+  public ResponseEntity<CommentPageResponse> getComments(
+      @RequestParam(required = false, defaultValue = "0") Integer page,
       @RequestParam(required = false, defaultValue = "") String sort) {
-    System.out.println(sort);
     if (sort.equals("like")) {
-      return commentService.getCommentsSortedByLikes(page);
+      return ResponseEntity.ok(commentService.getCommentsSortedByLikes(page));
     } else {
-      return commentService.getCommentsSortedByRecent(page);
+      return ResponseEntity.ok(commentService.getCommentsSortedByRecent(page));
     }
   }
 }
