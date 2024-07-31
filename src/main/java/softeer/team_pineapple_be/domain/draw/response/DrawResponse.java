8124cd2fab68828ac6491f212a9c1127bf5b7254
devchlee12 <devchlee12@gmail.com>
2024-07-31 15:32:21 +0900

    [FEAT] 경품 결과 공통 응답 객체 생성

---
+++ src/main/java/softeer/team_pineapple_be/domain/draw/response/DrawResponse.java
@@ -0,0 +1,13 @@
+package softeer.team_pineapple_be.domain.draw.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 경품 추첨 공통 응답
+ */
+@Getter
+@Setter
+public class DrawResponse {
+  Boolean isDrawWin;
+}
