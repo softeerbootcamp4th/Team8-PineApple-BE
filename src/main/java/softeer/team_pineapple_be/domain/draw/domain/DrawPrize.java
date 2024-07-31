73dcfd9588a772a6340c7fe4c879b49b24298297
devchlee12 <devchlee12@gmail.com>
2024-07-31 15:29:06 +0900

    [FEAT] 경품 QR 코드 저장 엔티티 추가

---
+++ src/main/java/softeer/team_pineapple_be/domain/draw/domain/DrawPrize.java
@@ -0,0 +1,32 @@
+package softeer.team_pineapple_be.domain.draw.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+/**
+ * 경품 QR코드나 바코드 사진 저장 엔티티
+ */
+@Entity
+@Getter
+public class DrawPrize {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String image;
+  private Boolean valid;
+  private String owner;
+  @ManyToOne
+  private DrawRewardInfo drawRewardInfo;
+
+  public void invalidate() {
+    this.valid = false;
+  }
+
+  public void isNowOwnedBy(String phoneNumber) {
+    this.owner = phoneNumber;
+  }
+}
