833608ecc807ded4d17b14cd3dad4c796f971a96
devchlee12 <devchlee12@gmail.com>
2024-07-31 15:27:41 +0900

    [FEAT] JPA Auditing 허용 설정

--- src/main/java/softeer/team_pineapple_be/TeamPineappleBeApplication.java
+++ src/main/java/softeer/team_pineapple_be/TeamPineappleBeApplication.java
@@ -2,7 +2,9 @@ package softeer.team_pineapple_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TeamPineappleBeApplication {
 
