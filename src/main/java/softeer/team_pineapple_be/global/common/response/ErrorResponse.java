228677a79cf2c57462491413f46f4b27ee806419
devchlee12 <devchlee12@gmail.com>
2024-08-01 16:20:36 +0900

    [FEAT] ErrorCode 에러 응답 객체 수정

--- src/main/java/softeer/team_pineapple_be/global/common/response/ErrorResponse.java
+++ src/main/java/softeer/team_pineapple_be/global/common/response/ErrorResponse.java
@@ -1,8 +1,38 @@
 package softeer.team_pineapple_be.global.common.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
  * 에러 응답
  */
-public record ErrorResponse(int status, String message) {
-}
+@Getter
+@RequiredArgsConstructor
+@Builder
+public class ErrorResponse {
+  private final String code;
+  private final String message;
 
+  @JsonInclude(JsonInclude.Include.NON_EMPTY)
+  private final List<ValidationError> errors;
+
+  @Getter
+  @Builder
+  @RequiredArgsConstructor
+  public static class ValidationError {
+
+    private final String field;
+    private final String message;
+
+    public static ValidationError of(final FieldError fieldError) {
+      return ValidationError.builder().field(fieldError.getField()).message(fieldError.getDefaultMessage()).build();
+    }
+  }
+}
