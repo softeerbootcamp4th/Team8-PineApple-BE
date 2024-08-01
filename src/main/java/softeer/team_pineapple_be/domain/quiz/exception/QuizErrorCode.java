20726687d4226724231ce729c81f612d6f327b0a
repitor222 <repitor15@gmail.com>
2024-08-01 18:07:05 +0900

    [feat] QuizErrorCode 에러 추가
    이미 참가한 사용자에 대한 PARTICIPATION_EXISTS추가

--- src/main/java/softeer/team_pineapple_be/domain/quiz/exception/QuizErrorCode.java
+++ src/main/java/softeer/team_pineapple_be/domain/quiz/exception/QuizErrorCode.java
@@ -10,7 +10,8 @@ import softeer.team_pineapple_be.global.exception.ErrorCode;
 public enum QuizErrorCode implements ErrorCode {
 
     NO_QUIZ_INFO(HttpStatus.BAD_REQUEST, "퀴즈 정답이 등록되어 있지 않습니다."),
-    NO_QUIZ_CONTENT(HttpStatus.BAD_REQUEST, "퀴즈 내용이 존재하지 않습니다.");
+    NO_QUIZ_CONTENT(HttpStatus.BAD_REQUEST, "퀴즈 내용이 존재하지 않습니다."),
+    PARTICIPATION_EXISTS(HttpStatus.FORBIDDEN, "이미 참여한 유저입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
