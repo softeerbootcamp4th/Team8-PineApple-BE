a9c15a23db592cddf14fcf7fcd0e9193dfb67103
repitor222 <repitor15@gmail.com>
2024-08-01 17:32:11 +0900

    [feat] QuizInfoRequest validation을 위한 설정
    @NotNull 설정 및 예외 메시지 추가

--- src/main/java/softeer/team_pineapple_be/domain/quiz/request/QuizInfoRequest.java
+++ src/main/java/softeer/team_pineapple_be/domain/quiz/request/QuizInfoRequest.java
@@ -1,15 +1,21 @@
 package softeer.team_pineapple_be.domain.quiz.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 퀴즈 정답 제출을 위한 클래스
  */
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class QuizInfoRequest {
 
+    @NotNull(message = "퀴즈 아이디는 필수입니다.")
     private Integer quizId;
+
+    @NotNull(message = "퀴즈 정답은 필수입니다.")
     private Byte answerNum;
 }
