ac9477793ff6d51f2a0c5e8c2a851d9970bc7d24
devchlee12 <devchlee12@gmail.com>
2024-08-01 16:18:21 +0900

    [FEAT] 인증코드 로그인 요청 검증

--- src/main/java/softeer/team_pineapple_be/domain/member/request/LoginAuthCodeRequest.java
+++ src/main/java/softeer/team_pineapple_be/domain/member/request/LoginAuthCodeRequest.java
@@ -1,18 +1,21 @@
 package softeer.team_pineapple_be.domain.member.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
- * 인증코드 요청
+ * 인증코드 로그인 요청
  */
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginAuthCodeRequest {
+  @NotNull(message = "핸드폰 번호는 필수입니다.")
   private String phoneNumber;
+  @NotNull(message = "코드는 필수입니다.")
   private Integer code;
 }
