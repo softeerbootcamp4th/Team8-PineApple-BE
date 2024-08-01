e3e824618296c6179ed117a5d0dcfe54b9dac5fa
repitor222 <repitor15@gmail.com>
2024-08-01 23:23:31 +0900

    [feat] CommentService의 예외 처리 설정
    작성된 에러 코드에 부합하는 예외 처리 설정

--- src/main/java/softeer/team_pineapple_be/domain/comment/service/CommentService.java
+++ src/main/java/softeer/team_pineapple_be/domain/comment/service/CommentService.java
@@ -16,13 +16,17 @@ import lombok.extern.slf4j.Slf4j;
 import softeer.team_pineapple_be.domain.comment.domain.Comment;
 import softeer.team_pineapple_be.domain.comment.domain.CommentLike;
 import softeer.team_pineapple_be.domain.comment.domain.id.LikeId;
+import softeer.team_pineapple_be.domain.comment.exception.CommentErrorCode;
 import softeer.team_pineapple_be.domain.comment.repository.CommentLikeRepository;
 import softeer.team_pineapple_be.domain.comment.repository.CommentRepository;
 import softeer.team_pineapple_be.domain.comment.request.CommentLikeRequest;
 import softeer.team_pineapple_be.domain.comment.request.CommentRequest;
 import softeer.team_pineapple_be.domain.comment.response.CommentPageResponse;
+import softeer.team_pineapple_be.domain.member.domain.Member;
+import softeer.team_pineapple_be.domain.member.exception.MemberErrorCode;
 import softeer.team_pineapple_be.domain.member.repository.MemberRepository;
 import softeer.team_pineapple_be.global.auth.service.AuthMemberService;
+import softeer.team_pineapple_be.global.exception.RestApiException;
 
 /**
  * 기대평 서비스
@@ -70,9 +74,13 @@ public class CommentService {
   @Transactional
   public void saveComment(CommentRequest commentRequest) {
     String memberPhoneNumber = authMemberService.getMemberPhoneNumber();
+    // 멤버 조회 로직이 다른 곳에 다 있어서 넣어뒀습니다.
+    // 코드 보시고 불필요하다 생각 드시면 삭제하셔도 됩니다.
+    // 아마 형님 로직은 phoneNumber 자체를 키로 둬서 안 쓰는 것 같은데 제꺼는 엔티티를 가져와야 해서 일단 넣어두겠습니다.
+    memberRepository.findByPhoneNumber(memberPhoneNumber)
+            .orElseThrow(()-> new RestApiException(MemberErrorCode.NO_MEMBER));
     if (wasMemberCommentedToday(memberPhoneNumber)) {
-      log.info("이미 기대평 작성");
-      return;
+      throw new RestApiException(CommentErrorCode.ALREADY_REVIEWED);
     }
     commentRepository.save(new Comment(commentRequest.getContent(), memberPhoneNumber));
   }
@@ -80,14 +88,20 @@ public class CommentService {
   @Transactional
   public void saveCommentLike(CommentLikeRequest commentLikeRequest) {
     String memberPhoneNumber = authMemberService.getMemberPhoneNumber();
+    // 멤버 조회 로직이 다른 곳에 다 있어서 넣어뒀습니다.
+    // 코드 보시고 불필요하다 생각 드시면 삭제하셔도 됩니다.
+    // 아마 형님 로직은 phoneNumber 자체를 키로 둬서 안 쓰는 것 같은데 제꺼는 엔티티를 가져와야 해서 일단 넣어두겠습니다.
+    memberRepository.findByPhoneNumber(memberPhoneNumber)
+            .orElseThrow(()-> new RestApiException(MemberErrorCode.NO_MEMBER));
     LikeId likeId = new LikeId(commentLikeRequest.getCommentId(), memberPhoneNumber);
     Optional<CommentLike> byId = commentLikeRepository.findById(likeId);
     if (byId.isPresent()) {
-      //TODO: 이미 좋아요 누름 예외처리
+      // 예외처리 로직이 아닌 취소 처리가 되어야 할 것 같아 남겨두었습니다.
+      //TODO: 이미 좋아요 누름 취소처리
       return;
     }
     Comment comment = commentRepository.findById(commentLikeRequest.getCommentId())
-                                       .orElseThrow(() -> new RuntimeException("이후 처리합시다"));
+                                       .orElseThrow(() -> new RestApiException(CommentErrorCode.NO_COMMENT));
     comment.increaseLikeCount();
     commentLikeRepository.save(new CommentLike(likeId));
   }
