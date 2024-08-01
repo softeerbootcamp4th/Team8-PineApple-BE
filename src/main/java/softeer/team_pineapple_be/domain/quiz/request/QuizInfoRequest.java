5ebec19f4d315a80ce43f0c75418e331731b12c4
repitor222 <repitor15@gmail.com>
2024-08-01 18:26:48 +0900

    [refactor] Min,Max -> Range 어노테이션 변경

--- src/main/java/softeer/team_pineapple_be/domain/quiz/request/QuizInfoRequest.java
+++ src/main/java/softeer/team_pineapple_be/domain/quiz/request/QuizInfoRequest.java
@@ -1,11 +1,10 @@
 package softeer.team_pineapple_be.domain.quiz.request;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 /**
  * 퀴즈 정답 제출을 위한 클래스
@@ -19,7 +18,6 @@ public class QuizInfoRequest {
     private Integer quizId;
 
     @NotNull(message = "{quiz.answer_required}")
-    @Min(value = 1, message = "{quiz.answer_num_range_message}")
-    @Max(value = 4, message = "{quiz.answer_num_range_message}")
+    @Range(min = 1, max = 4, message = "{quiz.answer_num_range_message}")
     private Byte answerNum;
 }
