9373aa90529dc95778d87173534c24238745b78b
repitor222 <repitor15@gmail.com>
2024-08-01 23:23:06 +0900

    [feat] Comment의 검증 조건 설정
    검증 조건 설정 및 예외 메세지 작성

--- src/main/java/softeer/team_pineapple_be/domain/comment/request/CommentRequest.java
+++ src/main/java/softeer/team_pineapple_be/domain/comment/request/CommentRequest.java
@@ -1,9 +1,11 @@
 package softeer.team_pineapple_be.domain.comment.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * 기대평 작성 요청 객체
@@ -13,5 +15,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentRequest {
+  @NotNull(message = "{comment.content}")
+  @Length(min = 1, max = 50, message = "{comment.content_length_range}")
   private String content;
 }
