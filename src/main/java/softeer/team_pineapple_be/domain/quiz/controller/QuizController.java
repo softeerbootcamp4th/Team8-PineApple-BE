2af18e4f7b4e334bd801f6deb00b3545fe441a50
devchlee12 <devchlee12@gmail.com>
2024-08-05 18:47:37 +0900

    [FEAT] 퀴즈 컨트롤러 중 인증 필요한 메소드에 어노테이션 추가

--- src/main/java/softeer/team_pineapple_be/domain/quiz/controller/QuizController.java
+++ src/main/java/softeer/team_pineapple_be/domain/quiz/controller/QuizController.java
@@ -1,17 +1,22 @@
 package softeer.team_pineapple_be.domain.quiz.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import softeer.team_pineapple_be.domain.member.response.MemberInfoResponse;
 import softeer.team_pineapple_be.domain.quiz.request.QuizInfoRequest;
 import softeer.team_pineapple_be.domain.quiz.response.QuizContentResponse;
 import softeer.team_pineapple_be.domain.quiz.response.QuizInfoResponse;
 import softeer.team_pineapple_be.domain.quiz.service.QuizService;
+import softeer.team_pineapple_be.global.auth.annotation.Auth;
 
 @Tag(name = "Quiz 관련 정보 제공", description = "퀴즈에 대한 처리(내용, 정답)")
 @RestController
@@ -19,24 +24,25 @@ import softeer.team_pineapple_be.domain.quiz.service.QuizService;
 @RequestMapping("/quiz")
 public class QuizController {
 
-    private final QuizService quizService;
+  private final QuizService quizService;
 
-    @Operation(summary = "퀴즈 내용 가져오기")
-    @GetMapping
-    public ResponseEntity<QuizContentResponse> getQuizContent() {
-        return ResponseEntity.ok().body(quizService.getQuizContent());
-    }
+  @Operation(summary = "퀴즈 내용 가져오기")
+  @GetMapping
+  public ResponseEntity<QuizContentResponse> getQuizContent() {
+    return ResponseEntity.ok().body(quizService.getQuizContent());
+  }
 
-    @Operation(summary = "퀴즈 정답 맞추기")
-    @PostMapping("/answer")
-    public ResponseEntity<QuizInfoResponse> isCorrect(@Valid @RequestBody QuizInfoRequest quizInfoRequest) {
-        return ResponseEntity.ok().body(quizService.quizIsCorrect(quizInfoRequest));
-    }
+  @Operation(summary = "퀴즈 정답 맞추기")
+  @PostMapping("/answer")
+  public ResponseEntity<QuizInfoResponse> isCorrect(@Valid @RequestBody QuizInfoRequest quizInfoRequest) {
+    return ResponseEntity.ok().body(quizService.quizIsCorrect(quizInfoRequest));
+  }
 
-    @Operation(summary = "퀴즈 참여 여부 등록")
-    @GetMapping("/participants")
-    public ResponseEntity<MemberInfoResponse> setQuizHistory(){
-        return ResponseEntity.ok().body(quizService.quizHistory());
-    }
+  @Auth
+  @Operation(summary = "퀴즈 참여 여부 등록")
+  @GetMapping("/participants")
+  public ResponseEntity<MemberInfoResponse> setQuizHistory() {
+    return ResponseEntity.ok().body(quizService.quizHistory());
+  }
 
 }
