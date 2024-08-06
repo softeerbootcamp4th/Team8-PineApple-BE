4f4eb2e4ab68636b652cd60881044d7501a9f168
gichan222 <repitor15@gmail.com>
2024-08-06 15:24:34 +0900

    [refactor] DrawPrize 클래스 AllArgsConstructor 생성
    테스트 코드 작성 시 엔티티 생성을 위한 생성자 생성

--- src/main/java/softeer/team_pineapple_be/domain/draw/domain/DrawPrize.java
+++ src/main/java/softeer/team_pineapple_be/domain/draw/domain/DrawPrize.java
@@ -5,13 +5,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 경품 QR코드나 바코드 사진 저장 엔티티
  */
 @Entity
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DrawPrize {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
