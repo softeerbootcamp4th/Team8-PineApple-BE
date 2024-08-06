7bafae56c874f2abbacafb3edee67366dfbb6788
gichan222 <repitor15@gmail.com>
2024-08-06 17:00:13 +0900

    [feat] DrawDailyMessageInfoRepository 의 메서드 반환 타입 optional로 수정
    예외 처리를 위한 반환타입 수정

--- src/main/java/softeer/team_pineapple_be/domain/draw/repository/DrawDailyMessageInfoRepository.java
+++ src/main/java/softeer/team_pineapple_be/domain/draw/repository/DrawDailyMessageInfoRepository.java
@@ -3,6 +3,7 @@ package softeer.team_pineapple_be.domain.draw.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import softeer.team_pineapple_be.domain.draw.domain.DrawDailyMessageInfo;
 
@@ -10,5 +11,5 @@ import softeer.team_pineapple_be.domain.draw.domain.DrawDailyMessageInfo;
  * 일자별 경품 추첨 메시지 리포지토리
  */
 public interface DrawDailyMessageInfoRepository extends JpaRepository<DrawDailyMessageInfo, Integer> {
-  public DrawDailyMessageInfo findByDrawDate(LocalDate drawDate);
+  Optional<DrawDailyMessageInfo> findByDrawDate(LocalDate drawDate);
 }
