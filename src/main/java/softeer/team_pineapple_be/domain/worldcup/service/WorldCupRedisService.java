ab2eb202523ce0d0a0630360d1a89c9a8b5a8686
devchlee12 <devchlee12@gmail.com>
2024-08-09 14:30:30 +0900

    [FEAT] 월드컵 투표 못받은 아이템 빈 생성시 초기화

--- src/main/java/softeer/team_pineapple_be/domain/worldcup/service/WorldCupRedisService.java
+++ src/main/java/softeer/team_pineapple_be/domain/worldcup/service/WorldCupRedisService.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import softeer.team_pineapple_be.domain.worldcup.response.WorldCupResultResponse;
 
@@ -17,6 +18,8 @@ import softeer.team_pineapple_be.domain.worldcup.response.WorldCupResultResponse
 @RequiredArgsConstructor
 @Service
 public class WorldCupRedisService {
+  private final static int WORLD_CUP_ANSWER_ID_START = 1;
+  private final static int WORLD_CUP_ANSWER_ID_END = 6;
   private final static String ANSWER_KEY = "worldcup_cnt";
   private final RedisTemplate<String, String> redisTemplate;
 
@@ -44,5 +47,16 @@ public class WorldCupRedisService {
    */
   public void increaseAnswerIdCount(Integer worldCupAnswerId) {
     redisTemplate.opsForZSet().incrementScore(ANSWER_KEY, String.valueOf(worldCupAnswerId), 1);
+  }
+
+  /**
+   * 월드컵 통계 초기화 -> 아직 투표를 못받은 아이템을 0으로 초기화
+   */
+  @PostConstruct
+  public void init() {
+    ZSetOperations<String, String> zSetOps = redisTemplate.opsForZSet();
+    for (int i = WORLD_CUP_ANSWER_ID_START; i <= WORLD_CUP_ANSWER_ID_END; i++) {
+      zSetOps.addIfAbsent(ANSWER_KEY, String.valueOf(i), 0);
+    }
   }
 }
