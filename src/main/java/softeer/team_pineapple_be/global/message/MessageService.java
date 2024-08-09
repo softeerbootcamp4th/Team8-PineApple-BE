9971731f6788dc42827a8ac2ebc9e15493935391
devchlee12 <devchlee12@gmail.com>
2024-08-09 13:34:31 +0900

    [FIX] 메시지 서비스 수정

--- src/main/java/softeer/team_pineapple_be/global/message/MessageService.java
+++ src/main/java/softeer/team_pineapple_be/global/message/MessageService.java
@@ -2,18 +2,24 @@ package softeer.team_pineapple_be.global.message;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import softeer.team_pineapple_be.global.auth.service.AuthMemberService;
 
 /**
  * 메시지 서비스 인터페이스
  */
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class MessageService {
+  private final AuthMemberService authMemberService;
+
   public boolean sendPrizeImage(String message) {
     //TODO : 이후 SDK 사용해서 경품 이미지 보내는 로직 구현해야함
     //TODO : S3에서 받아와서 보내기
-    log.info("경품 이미지 발송!");
+    String memberPhoneNumber = authMemberService.getMemberPhoneNumber();
+    log.info(memberPhoneNumber + "에게 경품 이미지 [" + message + "] 발송!");
     return true;
   }
 }
